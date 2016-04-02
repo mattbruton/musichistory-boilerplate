6b3006d7198b35effbbb94a13c79fc4bcae6a5f5
MATT BRUTON <the.bruton@gmail.com>
2016-04-02 08:17:53 -0500

    deleted old songs.js file, completed base songs2.json file

--- songs.js
+++
@@ -1,5 +0,0 @@
-var songs = [];
-
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-songs[songs.length] = ""
