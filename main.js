444e48dea4f6ae508369fe0643f01f1b762af1f9
MATT BRUTON <the.bruton@gmail.com>
2016-04-02 15:24:00 -0500

    made progress on creating ability to scroll through songs when too many are added for original container, still needs work. will push upper songs beyond scroll reach

--- main.js
+++ main.js
@@ -6,25 +6,42 @@ var songEl= document.getElementById('song');
 var albumEl= document.getElementById('album');
 var songAddBtn = document.getElementById('songAddBtn');
 
+// when app first loads, hides the addSongs element and displays the main content.
+
 $(document).ready(function() {
   mainContentElement.setAttribute("style", "display: flex");
   addSongsElement.setAttribute("style", "display:none");
 });
+
+// when Add Music button is pressed, hides main content area of page and shows Add Songs element.
 
 $("#addBtn").click(function() {
   mainContentElement.setAttribute("style", "display: none;");
   addSongsElement.setAttribute("style", "display: block");
 });
 
+// when List Music is pressed, hides the Add Songs element and displays Main Content area.
+
 $("#listBtn").click(  function() {
   mainContentElement.setAttribute("style", "display: flex");
   addSongsElement.setAttribute("style", "display:none");
 });
 
+// Removes Song if delete button is pressed.
+
+$(".songList").click(function(event) {
+  if (event.target.classList.contains("delBtn")){ 
+    event.target.parentNode.remove();
+  };
+});
+
+// function to add new song through 'Add Music' section of app.
+
 $("#songAddBtn").click(function() {
   var newSong = "";
-  newSong += `<div class="song">`;
+  newSong += `<div class="song" style="min-height: 90px;">`;
   newSong += `<a href="#">${songEl.value}</a>`;
+  newSong += `<button class="delBtn" type="">X</button>`;
   newSong += `<ul class="songInfo"`;
   newSong += `<li>${artistEl.value}</li>`;
   newSong += `<li class="songMid">${albumEl.value}</li>`;
@@ -33,12 +50,14 @@ $("#songAddBtn").click(function() {
   songList.innerHTML += newSong;
 });
 
+// Gets contents of song1.json and loads them when starting up app.
 
-/*<div class="song">
-            <a href="#">Song Name</a>
-            <ul class="songInfo">
-              <li>Artist Name</li>
-              <li class="songMid">Album Name</li>
-              <li>Genre</li>
-            </ul>
-          </div> */
+$.get('songs1.json', function(songs) {
+  for (var i = 0; i < songs.songs.length; i++) {
+    $("#songList")
+     .append(`<div class="song" style="min-height: 90px;"><a href="#">${songs.songs[i].title}</a><button class="delBtn" type="">X</button><ul class="songInfo"<li>Artist: ${songs.songs[i].artist}</li><li>Album:${songs.songs[i].album}</li></ul></div>`);
+  }
+}, "json");
+
+// waiting to fix flexbox before tackling the step where i add the second json file on click
+// $.get('songs2.json');
